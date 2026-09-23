30fc54f69b8448a21b35f8c9db1d4d1f505b1b0d
preethamkumarmutyala <165639030+preethamkumarmutyala@users.noreply.github.com>
2026-09-23 20:43:33 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,62 +1,31 @@
 document.getElementById("dashboard").innerHTML = `
-
 <div class="event">
 
-<div class="event-title">
-1000 BRM
-</div>
+    <div class="event-title">
+        1000 BRM
+    </div>
 
-<div class="section-title">
-DETAILS
-</div>
+    <div class="section-title">
+        DETAILS
+    </div>
 
-<div class="section-body">
+    <div class="section-body">
 
-<p>Registration Closes: 27-Sep-2026</p>
+        <p>Registration Closes: 27-Sep-2026 23:59</p>
 
-<a
-class="button green"
-target="_blank"
-href="https://ridewithgps.com/routes/52862280">
-VIEW ROUTE
-</a>
+        <a class="button green"
+           target="_blank"
+           href="https://ridewithgps.com/routes/52862280">
+           VIEW ROUTE
+        </a>
 
-<a
-class="button orange"
-target="_blank"
-href="https://www.audaxindia.in/event-e-10046">
-AUDAX PAGE
-</a>
+        <a class="button orange"
+           target="_blank"
+           href="https://www.audaxindia.in/event-e-10046">
+           AUDAX PAGE
+        </a>
 
-</div>
-
-<div class="section-title">
-FELLOW RIDERS
-</div>
-
-<div class="section-body">
-
-<table>
-
-<tr>
-<th>AIR</th>
-<th>Name</th>
-</tr>
-
-<tr>
-<td>8091</td>
-<td>Raghavendra Rasamsetty</td>
-</tr>
-
-<tr>
-<td>18511</td>
-<td>Girish Kumar Anem</td>
-</tr>
-
-</table>
+    </div>
 
 </div>
-
-</div>
-
 `;
